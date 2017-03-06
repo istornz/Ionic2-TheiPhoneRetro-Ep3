b0dbeab115b561b911a141a30207125bcf690cc9
Dimitri Dessus <iphoneretro@icloud.com>
2017-03-06 19:26:54 +0100

    [Add] First commit

---
+++ src/pages/details/details.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-details',
+  templateUrl: 'details.html'
+})
+export class DetailsPage {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
